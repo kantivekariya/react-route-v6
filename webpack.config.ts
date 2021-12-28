913f6f3aafb483444f22291620f607b740e52195
kevit-kanti-vekariya <kanti.vekariya@kevit.io>
2021-12-28 14:06:27 +0530

    initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,53 @@
+import path from "path";
+import { Configuration } from "webpack";
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+
+const config: Configuration = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.join(__dirname, "build"),
+    compress: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: {
+        files: "./src/**/*",
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
+
+export default config;
